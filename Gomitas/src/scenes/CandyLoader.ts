6b0e2a4d685e55acebfe0b5bbcd132ca66a81922
MarcosPimienta <fenix3819@gmail.com>
2022-09-03 22:23:25 -0500

    More control by creating animation groups

---
+++ Gomitas/src/scenes/CandyLoader.ts
@@ -0,0 +1,30 @@
+import * as BABYLON from 'babylonjs';
+import 'babylonjs-loaders';
+
+function boxController(meshNames:string[], meshPath:string, meshFile:string, scene:BABYLON.Scene){
+
+    BABYLON.SceneLoader.ImportMesh(meshNames, meshPath, meshFile, scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
+      const box = animationGroups[0];
+      const ls = newMeshes[0];
+      ls.translate(new BABYLON.Vector3(0, 0, 0), 1, BABYLON.Space.WORLD);
+      box.play(false);
+    });
+}
+
+function candiesLoader(meshNames:string[], meshPath:string, meshFile:string, scene:BABYLON.Scene){
+  BABYLON.SceneLoader.ImportMesh(meshNames, meshPath, meshFile, scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
+    //Animation groups that comes from a .glb file
+    const ChocoMellows = animationGroups[0];
+    const LifeSavers = animationGroups[1];
+    const Oranges = animationGroups[2];
+    const Ribbons = animationGroups[3];
+    const Strawberries = animationGroups[4];
+    const Worms = animationGroups[5];
+    const candyParent = newMeshes[0];
+    candyParent.translate(new BABYLON.Vector3(-1.56, 0, 0), 1, BABYLON.Space.WORLD);
+    ChocoMellows.play(false);
+  });
+}
+
+
+export { boxController, candiesLoader};
--- Gomitas/src/scenes/MyFirstScene.ts
+++ Gomitas/src/scenes/MyFirstScene.ts
@@ -11,35 +11,21 @@ import {
   SceneLoader
 } from "@babylonjs/core";
 import * as BABYLON from 'babylonjs';
-import { AnimationGroup } from "babylonjs";
 import 'babylonjs-loaders';
+import { boxController, candiesLoader } from "./CandyLoader";
+
 const createScene = (canvas) => {
   const engine = new Engine(canvas);
   const scene = new BABYLON.Scene(engine);
 
-  const camera = new ArcRotateCamera("camera1", 0, 10, 10, new Vector3(0, 5, -10), scene);
+  const camera = new ArcRotateCamera("camera1", 0, 0, 0, new Vector3(0, 5, 10), scene);
   camera.setTarget(Vector3.Zero());
   camera.attachControl(canvas, true);
 
   new HemisphericLight("light", Vector3.Up(), scene);
 
-  BABYLON.SceneLoader.ImportMesh("CandyBox", "./", "CandyBox.gltf", scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
-    const box = animationGroups[0];
-    box.play(false);
-});
-  BABYLON.SceneLoader.ImportMesh("", "./", "LifeSaversAnimation.glb", scene , function (newMeshes, particleSystems, skeletons, animationGroups) {
-    const ls = newMeshes[0];
-    const lifesaver = animationGroups[0];
-    lifesaver.play(false);
-    ls.translate(new BABYLON.Vector3(-1.57, 0, -0.95), 1, BABYLON.Space.WORLD);
-});
-
-  // const box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
-  /* const material = new StandardMaterial("box-material", scene);
-  material.diffuseColor = Color3.Blue();
-  box.material = material; */
-  //console.log(box.currentFrame);
-  //box.play();
+  boxController(["CandyBox"], "./", "CandyBox.gltf", scene);
+  candiesLoader(["LifeSavers" , "ChocoMellows", "Oranges", "Ribbons", "Strawberries", "Worms"], "./", "Candies.glb", scene);
 
   engine.runRenderLoop(() => {
     scene.render();
