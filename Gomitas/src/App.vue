166330ec34c9fa1e77ea12176d6313d18249a189
MarcosPimienta <fenix3819@gmail.com>
2022-11-09 22:00:28 -0500

    emits created

--- Gomitas/src/App.vue
+++ Gomitas/src/App.vue
@@ -12,10 +12,6 @@ export default {
   components: {
     MainScene,
   },
-
-  methods: {
-    
-  },
 };
 </script>
 
--- Gomitas/src/components/RadioButtons.vue
+++ Gomitas/src/components/RadioButtons.vue
@@ -5,43 +5,45 @@
         type="radio"
         value="ChocoMellows"
         name="ds"
-        @click="animSwitch(item)" />
+        @click="emit('animationPlay', item)" />
       <label class="label-text" for="ds">{{ item.name }}</label>
     </div>
   </div>
 </template>
-<script lang="ts">
-import { ref, onMounted } from "vue";
-import { objectScene } from "../scenes/Scene";
+<script setup lang="ts">
+import { ref, onMounted, onUpdated } from "vue";
 
-export default {
-  name: "RadioButtons",
+  const emit = defineEmits<{
+  (e: 'animationPlay', object: AnimState): void
+  }>();
 
-  setup() {
-    const animState = [
+    interface AnimState {
+      id: number;
+      name: string;
+      state: boolean;
+    };
+
+    const animState: AnimState[] = [
     { id: 0, name:"ChocoMellow", state: false },
     { id: 1, name:"LifeSavers", state: false },
     { id: 2, name:"Oranges", state: false },
   ];
 
     function animSwitch(item: any){
+      let animSelector: Number = 0;
       if(item.name == "ChocoMellow"){
-        console.log(item.name);
-        objectScene.getAnimation()
+        console.log(item.name + "anim " + anim);
       }
       else if(item.name == "LifeSavers"){
         console.log(item.name);
+        anim = 1;
       }
       else if(item.name == "Oranges"){
         console.log(item.name);
+        anim = 2;
       }
     };
 
-    return {
-      animState, animSwitch
-    }
-  },
-};
 </script>
 <style lang="css">
 @import "../assets/main.css";
