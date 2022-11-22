280ae126f721292853d78315b184e72f83d4df00
MarcosPimienta <fenix3819@gmail.com>
2022-11-21 22:34:03 -0500

    Spaces added to line

--- Gomitas/src/scenes/IleSelector.ts
+++ Gomitas/src/scenes/IleSelector.ts
@@ -6,7 +6,7 @@ function ilePlay( position: Vector3, rotation: number, scene: Scene){
 
   const plane = MeshBuilder.CreatePlane('select',{size: 1, width: 0.5, height: 1, sideOrientation: 2});
   plane.rotation.x = Math.PI/2;
-  plane.rotate(new Vector3(1, 1, 1), rotation*Math.PI, Space.WORLD);
+  plane.rotate(new Vector3(1, 1, 1), rotation * Math.PI, Space.WORLD);
   plane.translate(position, 1, Space.WORLD);
 }
 
