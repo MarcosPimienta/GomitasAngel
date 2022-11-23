c95d80928d4c5c937d37cfd63db4fe2e0ec87579
MarcosPimienta <fenix3819@gmail.com>
2022-11-23 11:39:05 -0500

    Plane selectors size and position adjusted

--- Gomitas/src/scenes/Scene.ts
+++ Gomitas/src/scenes/Scene.ts
@@ -27,7 +27,7 @@ const createScene = function (canvas:HTMLCanvasElement){
   camera.setTarget(Vector3.Zero());
   camera.attachControl(canvas, true);
   CandyLoader.boxController(["CandyBox"], "./", "CandyBox.gltf", scene);
-  IleSelector.ileLoad(new Vector3(-1.57, 0, 0), 90, scene);
+  IleSelector.ileLoad(new Vector3(-1.57, 0.3, 0), 90, scene);
   new HemisphericLight("light", Vector3.Up(), scene);
 
   scene.debugLayer.show({
