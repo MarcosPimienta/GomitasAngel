af49f1181a941e8d7895c41e7d6a1e1ec7ac701d
MarcosPimienta <fenix3819@gmail.com>
2023-08-18 16:39:14 -0500

    Reset Button added

---
+++ Gomitas/src/components/ResetButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="cart-btn" @click="resetCandies">Reset Candies</button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['reset']);
+
+const resetCandies = () => {
+  emit('reset');
+};
+
+</script>
