c8cddf993c2c0e9d910fdc395c9f90ce5cc55cc9
MarcosPimienta <1676@holbertonschool.com>
2023-08-18 19:43:58 -0500

    adding modal logic

--- Gomitas/src/components/CartModal.vue
+++ Gomitas/src/components/CartModal.vue
@@ -4,7 +4,7 @@
             <h2>Shopping Cart</h2>
             <p v-if="isIncomplete">Please select candies for each Ile to fill the box!</p>
             <ul v-else>
-                <li v-for="candy in selectedCandies" :key="candy.id">{{ candy.name }}</li>
+              <li v-for="name in candyNames" :key="name">{{ name }}</li>
             </ul>
         <button @click="closeModal">Close</button>
         </div>
@@ -12,20 +12,33 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps, defineEmits, computed } from 'vue';
 
-    props: ['show', 'selectedCandies'],
-    computed: {
-        isIncomplete() {
-        // Here, check if candies are selected for all Iles.
-        // Adjust this logic based on your application's data structure.
-        return this.selectedCandies.length < TOTAL_ILES;
-        }
-    },
-    methods: {
-        closeModal() {
-        this.$emit('close');
-        }
-    }
+// Define props
+const props = defineProps({
+    show: Boolean,
+    selectedCandies: Array,
+    allCandies: Array
+});
+
+const candyNames = computed(() => {
+  return props.selectedCandies?.map(candyId => {
+    const candy = props.allCandies?.find(c => c.id === candyId);
+    return candy ? candy.name : "Unknown Candy";
+  });
+});
+
+// Define emits
+const emit = defineEmits(['close']);
+
+// Computed property to check if candies are selected for all Iles
+const TOTAL_ILES = 6;  // Adjust this based on your requirement
+const isIncomplete = computed(() => props.selectedCandies.length < TOTAL_ILES);
+
+// Methods
+const closeModal = () => {
+    emit('close');
+}
 
 </script>
 
