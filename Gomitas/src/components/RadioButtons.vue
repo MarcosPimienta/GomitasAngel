fb1ba3248fb1d4529b6e9bc09666c652f2d2d4d3
MarcosPimienta <1676@holbertonschool.com>
2023-08-22 00:32:18 -0500

    Updates the radio buttons with mouse and arrows

--- Gomitas/src/components/RadioButtons.vue
+++ Gomitas/src/components/RadioButtons.vue
@@ -1,38 +1,51 @@
 <template>
   <div>
     <div v-for="item in animState" :key="item.id">
-      <input class="radio-btn" v-model="tutorials"
+      <input
+        class="radio-btn"
+        :value="item.id"
+        v-model="selectedCandyId"
         type="radio"
-        value="ChocoMellows"
-        name="ds"
-        @click="handleClick(item)" />
-      <label class="label-text" for="ds">{{ item.name }}</label>
+        :name="'ds_' + item.id"
+        @click="handleClick(item)"
+      />
+      <label class="label-text" :for="'ds_' + item.id">{{ item.name }}</label>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
+import type { PropType } from 'vue';
 
 const emit = defineEmits<{
   (e: 'animationPlay', object: AnimState): void;
   (e: 'candySelected', candyId: number): void;
 }>();
 
-    interface AnimState {
-      id: number;
-      name: string;
-      state: boolean;
-    };
+interface AnimState {
+  id: number;
+  name: string;
+  state: boolean;
+};
 
-    const animState: AnimState[] = [
-    { id: 0, name:"ChocoMellows", state: false },
-    { id: 1, name:"LifeSavers", state: false },
-    { id: 2, name:"Oranges", state: false },
-    { id: 3, name:"Ribbons", state: false },
-    { id: 4, name:"Strawberries", state: false },
-    { id: 5, name:"Worms", state: false },
-  ];
+const animState: AnimState[] = [
+  { id: 0, name:"ChocoMellows", state: false },
+  { id: 1, name:"LifeSavers", state: false },
+  { id: 2, name:"Oranges", state: false },
+  { id: 3, name:"Ribbons", state: false },
+  { id: 4, name:"Strawberries", state: false },
+  { id: 5, name:"Worms", state: false },
+];
 
-  function handleClick(item: AnimState) {
+// Define the new prop for the currently selected candy ID
+const props = defineProps({
+  selectedCandyId: {
+    type: [Number, Object] as PropType<number | null>,
+    default: null
+  }
+});
+
+function handleClick(item: AnimState) {
   emit('animationPlay', item);
   emit('candySelected', item.id);
 }
