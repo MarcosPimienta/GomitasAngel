cdad18bdb95506233149d7fd6a480a2f295d3435
MarcosPimienta <fenix3819@gmail.com>
2023-09-29 09:35:59 -0500

    Creating text input and dynammic text textures

---
+++ Gomitas/src/components/TextFields.vue
@@ -0,0 +1,24 @@
+<template>
+  <input v-model="dynamicText" @input="updateTexture" />
+</template>
+<script>
+  export default {
+    data() {
+      return {
+            dynamicText: "",
+            dynamicTexture: null, // Assume this is somehow set initially
+          };
+      },
+
+      methods: {
+        updateTexture() {
+          if (this.dynamicTexture) {
+            const ctx = this.dynamicTexture.getContext();
+            ctx.clearRect(0, 0, 512, 512); // Clear previous text
+            ctx.fillText(this.dynamicText, 256, 256); // Draw new text
+            this.dynamicTexture.update(); // Update the texture
+          }
+        },
+    },
+  };
+      </script>
