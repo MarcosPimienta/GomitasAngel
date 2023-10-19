bf57ad4268620e7b95d193cc41bb3785c17a9bf4
MarcosPimienta <fenix3819@gmail.com>
2023-10-19 10:44:55 -0500

    both texts now are independent fromm each other

---
+++ Gomitas/src/components/MessageField.vue
@@ -0,0 +1,33 @@
+<template>
+  <label class="label-text">Message</label>
+  <input class="name-field" v-model="dynamicText" @input="updateTexture" />
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    onTextUpdated: {
+      type: Function as PropType<(text: string) => void>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const dynamicText = ref('');
+
+    const updateTexture = () => {
+      console.log('Updating texture with message:', dynamicText.value);
+      props.onTextUpdated(dynamicText.value);
+    };
+
+    return {
+      dynamicText,
+      updateTexture,
+    };
+  },
+});
+</script>
+<style lang="css">
+@import "../assets/main.css";
+</style>
--- Gomitas/src/scenes/CandyLoader.ts
+++ Gomitas/src/scenes/CandyLoader.ts
@@ -224,7 +224,7 @@ function candiesLoader(scene: Scene, position: Vector3){
 
 // const path = `static/models/${candy.name}.glb`;
 
-function boxController(
+/* function boxController(
   meshNames: string[],
   meshPath: string,
   meshFile: string,
@@ -272,7 +272,11 @@ function boxController(
     }
   );
 
+  let currentNameText = '';
+  let currentMessageText = '';
+
   const updateText = (newText: string) => {
+    currentNameText = newText;
     if (!dynamicTexture) return;
 
     const textureContext = dynamicTexture.getContext();
@@ -302,7 +306,53 @@ function boxController(
       // Ensure the custom font is loaded before drawing text with it
       const drawTextWithCustomFont = () => {
         const font = "bold 88px Simplicity";
-        dynamicTexture.drawText(newText, 350, 600, font, "white", null, true, true);
+        dynamicTexture.drawText(currentNameText, 350, 600, font, "white", null, true, true);
+
+        // Reset the transformation to draw other elements normally
+        textureContext.setTransform(1, 0, 0, 1, 0, 0);
+        dynamicTexture.update();
+      };
+
+      if (document.fonts) {
+        document.fonts.load('1em Simplicity').then(drawTextWithCustomFont);
+      } else {
+        drawTextWithCustomFont();
+      }
+    };
+  };
+
+  const updateMsg = (newText: string) => {
+    currentMessageText = newText;
+    if (!dynamicTexture) return;
+
+    const textureContext = dynamicTexture.getContext();
+    const textureResolution = 1024;
+
+    // Load the background image
+    const img = new Image();
+    img.src = './textures/Outer_Box_Texture.png';
+
+    img.onload = () => {
+      // Draw the background image onto the dynamic texture
+      textureContext.drawImage(img, 0, 0, textureResolution, textureResolution);
+
+      // Flip the canvas vertically
+      textureContext.translate(0, textureResolution);
+      textureContext.scale(1, -1);
+
+      // Rotation
+      const x = 400;
+      const y = textureResolution - 400;
+      const angleInRadians = Math.PI / 0;
+
+      textureContext.translate(x, y);
+      textureContext.rotate(-angleInRadians);
+      textureContext.translate(-x, -y);
+
+      // Ensure the custom font is loaded before drawing text with it
+      const drawTextWithCustomFont = () => {
+        const font = "bold 88px Simplicity";
+        dynamicTexture.drawText(currentMessageText, 350, 800, font, "white", null, true, true);
 
         // Reset the transformation to draw other elements normally
         textureContext.setTransform(1, 0, 0, 1, 0, 0);
@@ -318,7 +368,110 @@ function boxController(
   };
 
   return {
-    updateText  // Expose the updateText method
+    updateText,  // Expose the updateText method
+    updateMsg  // Expose the updateMessage method
+  };
+} */
+
+function boxController(
+  meshNames: string[],
+  meshPath: string,
+  meshFile: string,
+  scene: Scene
+) {
+  let dynamicTexture: DynamicTexture;
+  let currentNameText = '';
+  let currentMessageText = '';
+
+  const textureResolution = 1024; // Added this line
+
+  SceneLoader.ImportMesh(
+    meshNames,
+    meshPath,
+    meshFile,
+    scene,
+    (newMeshes, particleSystems, skeletons, animationGroups) => {
+      const innerCover = newMeshes.find((mesh) => mesh.name === "Inner_Cover");
+      if (innerCover && innerCover.material) {
+        const pbrMaterial = innerCover.material as PBRMaterial;
+        dynamicTexture = new DynamicTexture("dynamic texture", textureResolution, scene);
+        pbrMaterial.albedoTexture = dynamicTexture;
+
+        const img = new Image();
+        img.src = './textures/Outer_Box_Texture.png';
+
+        img.onload = () => {
+          dynamicTexture.getContext().drawImage(img, 0, 0, textureResolution, textureResolution);
+          dynamicTexture.update();
+        };
+      } else {
+        console.log("Inner_Cover mesh or material not found");
+      }
+
+      const box = newMeshes[0];
+      const startBox = animationGroups[0];
+      box.translate(new Vector3(0, 0, 0), 1, Space.WORLD);
+      startBox.play(false);
+    }
+  );
+
+  const redrawTexts = () => {
+    if (!dynamicTexture) return;
+
+    const textureContext = dynamicTexture.getContext();
+
+    const img = new Image();
+    img.src = './textures/Outer_Box_Texture.png';
+
+    img.onload = () => {
+      textureContext.drawImage(img, 0, 0, textureResolution, textureResolution);
+
+      drawText(currentNameText, 600);
+      drawText(currentMessageText, 800);
+      dynamicTexture.update();
+    };
+  };
+
+  const drawText = (text: string, positionY: number) => {
+    if (!dynamicTexture) return;
+
+    const textureContext = dynamicTexture.getContext();
+
+    const x = 400;
+    const y = positionY;
+
+    const drawTextWithCustomFont = () => {
+      const font = "bold 88px Simplicity";
+      textureContext.save();
+      textureContext.translate(x, textureResolution - y);
+      textureContext.scale(1, -1);  // This line is added to flip the text
+      dynamicTexture.drawText(text, 0, 0, font, "white", null, true, true);
+      textureContext.restore();
+
+      // Reset the transformation to draw other elements normally
+      textureContext.setTransform(1, 0, 0, 1, 0, 0);
+    };
+
+    if (document.fonts) {
+      document.fonts.load('1em Simplicity').then(drawTextWithCustomFont);
+    } else {
+      drawTextWithCustomFont();
+    }
+  };
+
+  const updateText = (newText: string) => {
+    currentNameText = newText;
+    redrawTexts();
+  };
+
+  const updateMsg = (newText: string) => {
+    currentMessageText = newText;
+    redrawTexts();
+  };
+
+  return {
+    updateText,
+    updateMsg
   };
 }
 
