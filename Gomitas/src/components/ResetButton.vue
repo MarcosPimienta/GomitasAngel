5c23e4546f3fb437f0707c87ce96c9b56711b022
MarcosPimienta <1676@holbertonschool.com>
2023-10-22 23:53:39 -0500

    IleButtons and ResetButtons disabled on close box

--- Gomitas/src/components/ResetButton.vue
+++ Gomitas/src/components/ResetButton.vue
@@ -1,10 +1,14 @@
 <template>
-  <button class="cart-btn" @click="resetCandies">Reset Candies</button>
+  <button :disabled="disabled" class="cart-btn" @click="resetCandies">Reset Candies</button>
 </template>
 
 <script setup lang="ts">
 
 const emit = defineEmits(['reset']);
+
+const props = defineProps({
+    disabled: Boolean,
+  });
 
 const resetCandies = () => {
   emit('reset');
