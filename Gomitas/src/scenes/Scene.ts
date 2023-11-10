3f2a7c35d41d95a4d311613d6c13cbd9ec37ca60
MarcosPimienta <fenix3819@gmail.com>
2023-11-10 11:49:23 -0500

    folder renamed

--- Gomitas/src/scenes/Scene.ts
+++ Gomitas/src/scenes/Scene.ts
@@ -33,7 +33,7 @@ const createScene = function (canvas:HTMLCanvasElement, onIleSelected: (index: n
   //camera.minZ = 0.1;
 
   //load candy box with open animation
-  let box = CandyLoader.boxController(["CandyBox"], "./models/", "CandyBox.gltf", scene);
+  let box = CandyLoader.boxController(["CandyBox"], "./3d-models/", "CandyBox.gltf", scene);
   // load knot with animation
   let knot = CandyLoader.knotController(["Cinta"], "./models/", "Nudo_Cinta.gltf", scene);
   //console.log(box,);
